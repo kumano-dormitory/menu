cb1d39e350fc88307c0ff973c9006070ccb7976a
Hayato Hashimoto <hayato.hashimoto@gmail.com>
2017-03-04 22:11:33 +0900

    マウントポイント db/migrate を作るようにした

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-mkdir /app
+mkdir -p /app
 mount -t tmpfs tmpfs /app
 mkdir /app/workdir
 mkdir /app/mountpoint
 mount -t overlay overlay -o lowerdir=/app_readonly,upperdir=/app/mountpoint,workdir=/app/workdir /app/mountpoint
 cd /app/mountpoint
 mount --bind /var/db db
+mkdir -p db/migrate
 mount --bind /app_readonly/db/migrate db/migrate
 rake db:migrate
 rails server -b 0.0.0.0
